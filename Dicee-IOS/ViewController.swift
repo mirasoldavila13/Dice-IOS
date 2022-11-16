95a8e801fe2e1966e85e85ee742032b2ea39a69b
mirasoldavila13 <mirasoldavila13@aim.com>
2022-11-15 23:12:04 -0800

    changed dice to six manually

--- Dicee-IOS/ViewController.swift
+++ Dicee-IOS/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
     }
 
 
