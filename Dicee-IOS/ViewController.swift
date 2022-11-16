16235fd37ed195b6dea52f7b56918a174fd5d9a0
mirasoldavila13 <mirasoldavila13@aim.com>
2022-11-15 23:22:20 -0800

    changed dice 1 to dice2

--- Dicee-IOS/ViewController.swift
+++ Dicee-IOS/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
+        
     }
 
 
