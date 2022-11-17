874129c15052e962f681cce6e8f9e9d5a6a86a37
mirasoldavila13 <mirasoldavila13@aim.com>
2022-11-16 21:23:21 -0800

    randomize dice

--- Dicee-IOS/ViewController.swift
+++ Dicee-IOS/ViewController.swift
@@ -15,16 +15,12 @@ class ViewController: UIViewController {
     var leftDiceNumber = 1
     var rightDiceNumber = 5
     
-    override func viewDidLoad() {
-        
-        
-    }
-
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][rightDiceNumber]
+        diceImageView1.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 0...5)]
+        diceImageView2.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 0...5)]
         leftDiceNumber += 1
         rightDiceNumber -= 1
+        
     }
     
 }
