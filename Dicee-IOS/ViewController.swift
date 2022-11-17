b5a720184328ab24e17fdd720edb2f65800be4a7
mirasoldavila13 <mirasoldavila13@aim.com>
2022-11-16 19:07:34 -0800

    create array for dice2 and decremented the array

--- Dicee-IOS/ViewController.swift
+++ Dicee-IOS/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     var leftDiceNumber = 1
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         
@@ -21,7 +22,9 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         diceImageView1.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][rightDiceNumber]
         leftDiceNumber += 1
+        rightDiceNumber -= 1
     }
     
 }
